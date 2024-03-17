52737bbf582109423d15efe2d9e8d78bd7a51a57
Woualuimited <marius.malot.th@gmail.com>
2024-03-17 05:12:00 +0100

    created board constructor

--- Chess/board.cpp
+++ Chess/board.cpp
@@ -1,3 +1,15 @@
 #include "board.h"
 
-Board::Board() {}
+Board::Board()
+    : pieces(), takenPieces()
+{
+    //TODO add all pieces
+    Position e1(5, 1);
+    Piece whiteKing("whiteKing", "white", e1);
+
+    Position e8(5, 8);
+    Piece blackKing("blackKing", "black", e1);
+
+    pieces.append(whiteKing);
+    pieces.append(blackKing);
+}
