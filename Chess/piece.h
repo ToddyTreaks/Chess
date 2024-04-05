e3d687ac2a494873dcf18c134439db98572587cc
Thomien Leygonie <124178882+ToddyTreaks@users.noreply.github.com>
2024-04-05 22:27:40 +0200

    add comment to piece and position

--- Chess/piece.h
+++ Chess/piece.h
@@ -12,22 +12,72 @@ class MovementStrategy;
 class Piece
 {
 public:
+    /*
+    * Constructors
+    */
     Piece();
     Piece(const Piece &piece);
     Piece(QString color, QString pgnIdentifier, Position position);
+    /*
+    * Destructor
+    */
     ~Piece();
 
+    /*
+    * Attributes 
+    */
     Position position;
 
+    /*
+    * @return the color of the piece
+    */
     QString getColor();
+    
+    /*
+    * @return the PGN identifier of the piece
+    */
     QString getPgnIdentifier();
+    
+    /*
+    * @return the icon file name of the piece
+    */
     QString getIconFileName();
 
+    /*
+    * @return the attributes as a string
+    */
     QString toString();
 
+    
+    /*
+    * @param position: the position to move to
+    * @param positions: the positions of the pieces on the board
+    * @param QMap<Position, Piece>: a map of the pieces on the board that use the position as key
+    * @raise nothing
+    * @return true if the piece can move to the position, false otherwise
+    * @note check if the pice canGoTo the position and verify prerequisites if needed
+    */
     bool matchPosition(const Position &nextPosition, Position prerequisite, const QMap<Position, Piece> &pieces);
 
+    
+    /*
+    * @param QString: the png identifier of the piece
+    * @param QString: the color of the piece
+    * @param QMap<Position, Piece>: a map of the pieces on the board that use the position as key
+    * @raise nothing
+    * @return the piece with the given pgn identifier and color
+    */
     static Piece findPiece(QString pgnIdentifier, QString color, const QMap<Position, Piece> &pieces);
+    
+    /*
+    * @param QString: the png identifier of the piece
+    * @param QString: the color of the piece
+    * @param Position: the next position of the piece
+    * @param Position: the prerequisite position of the piece
+    * @param QMap<Position, Piece>: a map of the pieces on the board that use the position as key
+    * @raise nothing
+    * @return the piece with the given pgn identifier and color
+    */
     static Piece findPiece(QString pgnIdentifier, QString color, const Position &nextPosition, Position prerequisite, const QMap<Position, Piece> &pieces);
 
 protected:
--- Chess/position.h
+++ Chess/position.h
@@ -8,18 +8,47 @@ class Piece;
 class Position
 {
 public:
+
+    /*
+    * Constructors
+    */
     Position();
     Position(int row, int column);
 
+    /*
+    * Attributes
+    */
     int row;
     int column;
-
+    /*
+    * @return the position in the form of a string 
+    */
     QString toString() const;
+    /*
+    * @param pieces: the pieces on the board
+    * @raise nothing
+    * @return true if the position is empty, false otherwise
+    */
     bool isEmpty(const QMap<Position, Piece> &pieces) const;
+    /*
+    * @param color: the color of the piece
+    * @param QMap<Position, Piece>: a map of the pieces on the board that use the position as key
+    * @raise nothing
+    * @return true if the position is empty, false otherwise
+    */
     bool isEmpty(QString color, const QMap<Position, Piece> &pieces) const;
 
+    /*
+    * @param position: the position to compare
+    */
     bool operator<(const Position& other) const;
+    /*
+    * @param position: the position to compare
+    */    
     bool operator==(const Position& other) const;
+    /*
+    * @param position: the position to compare
+    */  
     bool operator!=(const Position& other) const;
 
 };
